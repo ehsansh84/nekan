70342080e44ed525ffa43438da2f1e0c313ce268
ehsan <ehsan.shirzadi@gmail.com>
2022-04-10 16:35:24 +0430

    login prototype

--- handlers/handlers.py
+++ handlers/handlers.py
@@ -24,7 +24,53 @@ class Login(BaseHandler):
                 self.success()
             else:
                 self.fail()
+            self.allow_action = False
         except:
             log.error(f'An error occurred! {ExceptionLine()}')
 
-    
+
+class Student(BaseHandler):
+    def init_method(self):
+        self.required = {
+            'post': ['name', 'family', 'passport_no', 'gender', 'country']
+        }
+        self.inputs = {
+            # 'post': ['field1', 'field2', 'field3'],
+            # 'put': ['field1', 'field2', 'field3']
+        }
+        self.tokenless = True
+
+
+    def before_post(self):
+        try:
+            log.debug(self.params)
+            return True
+        except:
+            log.error(f'An error occurred! {ExceptionLine()}')
+
+
+class Country(BaseHandler):
+    def init_method(self):
+        self.required = {
+            # 'post': ['name', 'family', 'passport_no', 'gender', 'country']
+        }
+        self.inputs = {
+            # 'post': ['field1', 'field2', 'field3'],
+            # 'put': ['field1', 'field2', 'field3']
+        }
+        self.tokenless = True
+
+    def before_get(self):
+        try:
+            # log.debug(self.params)
+            self.output['data']['list'] = [
+                {'id': 1, 'name': 'Iran'},
+                {'id': 2, 'name': 'Afghanistan'},
+                {'id': 3, 'name': 'USA'},
+                {'id': 4, 'name': 'Germany'}
+            ]
+            self.allow_action = False
+            return True
+        except:
+            log.error(f'An error occurred! {ExceptionLine()}')
+
--- urls.py
+++ urls.py
@@ -1,6 +1,8 @@
-from handlers.handlers import Login
+from handlers.handlers import Login, Student, Country
 
 
 url_patterns = [
     ("/v1/login/?([^/]+)?", Login, None, "login"),
+    ("/v1/student/?([^/]+)?", Student, None, "student"),
+    ("/v1/country/?([^/]+)?", Country, None, "country"),
 ]
