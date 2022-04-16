6a5285db11285584921db16ec3354d77d410bb72
ehsan <ehsan.shirzadi@gmail.com>
2022-04-16 22:45:16 +0430

    added upload

--- urls.py
+++ urls.py
@@ -1,8 +1,10 @@
 from handlers.handlers import Login, Student, Country
+from handlers.upload import Upload
 
 
 url_patterns = [
     ("/v1/login/?([^/]+)?", Login, None, "login"),
     ("/v1/student/?([^/]+)?", Student, None, "student"),
     ("/v1/country/?([^/]+)?", Country, None, "country"),
+    ("/v2/upload", Upload, None, "upload_v2"),
 ]
